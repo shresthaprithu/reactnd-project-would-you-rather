d97a99db3e41e43b365aa5c9ddab195dc990cd0a
Prithu Shrestha <shresthaprithu@gmail.com>
2019-10-27 20:52:22 +1100

    feat: created actions and action creators

---
+++ src/actions/questions.js
@@ -0,0 +1,9 @@
+// questions.js
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions
+  };
+}
---
+++ src/actions/shared.js
@@ -0,0 +1,13 @@
+// shared.js
+import { getInitialData } from '../utils/api';
+import { receiveQuestions } from '../actions/questions';
+import { receiveUsers } from '../actions/users';
+
+export function handleInitialData() {
+  return dispatch => {
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(receiveQuestions(questions));
+      dispatch(receiveUsers(users));
+    });
+  };
+}
