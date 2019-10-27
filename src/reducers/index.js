eb65d5612afaeb367422c7928e2f91da46df72ed
Prithu Shrestha <shresthaprithu@gmail.com>
2019-10-27 21:10:50 +1100

    feat: created reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+// index.js
+import { combineReducers } from 'redux';
+import authUser from '../reducers/authUser';
+import questions from '../reducers/questions';
+import users from '../reducers/users';
+
+export default combineReducers({
+  authUser,
+  questions,
+  users
+});
